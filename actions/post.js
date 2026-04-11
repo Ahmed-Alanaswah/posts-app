038a0def2e73a038e54172ab3f0647616c81dd01
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2026-04-11 19:32:39 +0300

    finish

--- actions/post.js
+++ actions/post.js
@@ -1,7 +1,8 @@
 "use server";
 import { redirect } from "next/navigation";
-import { storePost } from "@/lib/posts";
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
+import { revalidatePath } from "next/cache";
 export async function createPost(prevState, formData) {
   const title = formData.get("title");
   const content = formData.get("content");
@@ -33,5 +34,11 @@ export async function createPost(prevState, formData) {
     throw new Error("Image upload failed...");
   }
   await storePost({ imageUrl, title, content, userId: 1 });
+  revalidatePath("/", "layout");
   redirect("/feed");
+}
+
+export async function togglePostLikeStatus(postId) {
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath("/", "layout");
 }
